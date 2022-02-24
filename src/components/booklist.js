fbbae8260f728d86f359b0b0ea35ec2d938ee4a3
gdumani <gdumani@gmail.com>
2022-02-24 12:30:59 -0600

    configure booklist to retrieve from store

--- src/components/booklist.js
+++ src/components/booklist.js
@@ -1,37 +1,21 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/jsx-key */
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './book';
 
-const BOOKDATA = [
-  {
-    id: 1,
-    title: 'The Hunger Game',
-    author: 'Suzanne Collins',
-    category: 'action',
-  },
-  {
-    id: 2,
-    title: 'Dune',
-    author: 'Frank Herbert',
-    category: 'science fiction',
-  },
-  {
-    id: 3,
-    title: 'Capital in the Twenty-First Century',
-    author: 'Suzanne Collins',
-    category: 'economy',
-  },
-];
-
-const Booklist = () => (
-  <div>
-    <ul>
-      {BOOKDATA.map((book) => (
-        <Book {...book} />
-      ))}
-    </ul>
-  </div>
-);
+const Booklist = () => {
+  const books = useSelector((state) => state.booksReducer);
+  console.log(books);
+  return (
+    <div>
+      <ul>
+        {books.map((book) => (
+          <Book {...book} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Booklist;
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,7 +1,26 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
-const initialState = [];
+const initialState = [
+  {
+    id: 1,
+    title: 'The Hunger Game',
+    author: 'Suzanne Collins',
+    category: 'action',
+  },
+  {
+    id: 2,
+    title: 'Dune',
+    author: 'Frank Herbert',
+    category: 'science fiction',
+  },
+  {
+    id: 3,
+    title: 'Capital in the Twenty-First Century',
+    author: 'Suzanne Collins',
+    category: 'economy',
+  },
+];
 
 export const addBook = (payload) => ({
   type: ADD_BOOK,
